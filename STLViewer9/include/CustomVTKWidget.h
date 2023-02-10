515c1e2aa5ff8d25c645f2c008e0dfd5e172541e
choieunchong <zlqh1997@gmail.com>
2023-02-10 19:11:40 +0900

    Texturemapping

---
+++ STLViewer9/include/CustomVTKWidget.h
@@ -0,0 +1,42 @@
+#pragma once
+#include <QVTKOpenGLNativeWidget.h>
+#include <QVTKInteractorAdapter.h>
+#include <vtkSTLReader.h>
+#include <vtkActor.h>
+#include "CustomInteractorStyle.h"
+#include <vtkPolyData.h>
+#include <vtkSmartPointer.h>
+#include "observer.h"
+#include <vtkOrientationMarkerWidget.h>
+#include <vtkAxesActor.h>
+#include <QDebug>
+
+class CustomVTKWidget : public QVTKOpenGLNativeWidget , public Observer
+{ 
+    Q_OBJECT
+        
+
+public:
+    CustomVTKWidget();
+	CustomVTKWidget(QWidget* parent);
+	~CustomVTKWidget();
+
+    // Renderer Add Actor
+    void AddActor(vtkSmartPointer<vtkActor>);
+    void GetPolyData(vtkSmartPointer<vtkPolyData>);
+    void GetSphere(vtkSmartPointer<vtkSphereSource>);
+    void AddSphere(vtkSmartPointer<vtkActor>);
+
+
+protected:
+    // VTK Renderer
+    vtkSmartPointer<vtkRenderer> mRenderer; 
+    // VTK Render Window
+    vtkSmartPointer<vtkGenericOpenGLRenderWindow> mRenderWindow; 
+    vtkSmartPointer<QVTKInteractor> mInteractor;
+    QVTKInteractorAdapter* mvtkInteractorAdapter; 
+    virtual void func(vtkSmartPointer<vtkActor>);
+
+private: 
+    CustomInteractorStyle* customInteractorStyle;
+};
